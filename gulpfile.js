b3a37609e50d0b0136120667b35d2ac52999836c
thomasrcezar <thomasrbcezar@gmail.com>
2020-05-17 12:08:19 -0300

    Configurando browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,56 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
-let uglify = require('gulp-uglify-es').default;
-const watch = require('gulp-watch');
-const gls =require('gulp-live-server');
+const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync')
+
+
+gulp.task('browserSync', function(){
+    browserSync({
+        server:{
+            baseDir: "../portfolio_engenheira_civil/"
+            
+        },
+        options:{
+            reloadDelay:250
+        },
+        notify: false
+    });
+})
+
+
 
 
 sass.compiler = require('node-sass');
-
-
-
 
 gulp.task('sass', () => {
       return gulp.src('./css/_scss/**/*.scss')
       .pipe(concat('style.min.css'))
       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-      .pipe(gulp.dest('./css'));
+      .pipe(gulp.dest('./css'))
+      .pipe(browserSync.stream())
+
    });
 
    gulp.task('js',() =>{
        return gulp.src('./js/_js/**/*.js')
        .pipe(concat('js.min.js'))
        .pipe(uglify())
-       .pipe(gulp.dest('./js'));
+       .pipe(gulp.dest('./js'))
+       .pipe(browserSync.stream());
    });
 
-   gulp.task('watch',() =>{
-       gulp.watch('./js/_js/**/*.js'.gulp.series('js'))
-       gulp.watch('/css/_scss/**/*.scss'.gulp.series('sass'))
-       
+  
+
+   
+
+
+
+
+
+    gulp.task('default',gulp.parallel('browserSync','sass','js'), function(){
+    gulp.watch('./js/_js/**/*.js', ['js']);
+    gulp.watch('./css/_scss/**/*.scss', ['sass']);
+
+
    });
-
-   gulp.task('serve',()=>{
-       var server = gls.static('./',8000);
-       server.start();
-       gulp.watch('./css/**/*.css', function(file){
-        gls.notify.apply(server,[file]);
-    });
-
-    gulp.watch('./js/**/*.js', function(file){
-        gls.notify.apply(server,[file]);
-    });
-
-    gulp.watch('./index.html', function(file){
-        gls.notify.apply(server,[file]);
-    });
-
-   })
-
-
-
-
-
-   gulp.task('default',gulp.series('sass','js','serve','watch'));
