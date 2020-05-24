75eee23c0966e23edffeef9e223b4f3317da77af
thomasrcezar <thomasrbcezar@gmail.com>
2020-05-24 17:49:24 -0300

    Ajustando browsersynv

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,11 @@ gulp.task('browserSync', function(){
         },
         notify: false
     });
+
+    // gulp.watch('./js/_js/**/*.js').on('change',browserSync.reload)
+    // gulp.watch('./css/_scss/**/*.scss').on('change',browserSync.reload);
+    // gulp.watch("./*.html").on('change',browserSync.reload);
+
 })
 
 
@@ -48,10 +53,14 @@ gulp.task('sass', () => {
 
 
 
-    gulp.task('default',gulp.parallel('browserSync','sass','js'), function(){
-    gulp.watch('./js/_js/**/*.js', gulp.series('js'));
-    gulp.watch('./css/_scss/**/*.scss', ['sass']);
-    gulp.watch("./*.html").on('change',browserSync.reload);
+    gulp.task('default',gulp.parallel('browserSync','sass','js',function(done){
+        
+        gulp.watch('./js/_js/**/*.js', gulp.series('js'));
+        gulp.watch('./css/_scss/**/*.scss',gulp.series('sass'));
+        gulp.watch("./*.html").on('change',browserSync.reload);
+
+        done();
+    })
 
 
-   });
+   );
